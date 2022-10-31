7aaf9078d8a30fef70ad248891afb1b3ea600a3d
Sam <sam7k9621@gmail.com>
2022-10-31 18:50:42 +0800

    First draft of my thesis

--- compiler.sh
+++ compiler.sh
@@ -3,14 +3,14 @@
 rm -rf output
 mkdir output
 
-pdflatex -synctex=1 -interaction=nonstopmode PhDThesis.tex > compile1.log 2>&1 
+pdflatex -synctex=1 -interaction=nonstopmode PhD.tex > compile1.log 2>&1 
 
-biber PhDThesis
+biber PhD
 
-pdflatex -synctex=1 -interaction=nonstopmode PhDThesis.tex > compile2.log 2>&1
-pdflatex -synctex=1 -interaction=nonstopmode PhDThesis.tex
+pdflatex -synctex=1 -interaction=nonstopmode PhD.tex > compile2.log 2>&1
+pdflatex -synctex=1 -interaction=nonstopmode PhD.tex
 
-FILELIST=($(ls -d PhDThesis*))
+FILELIST=($(ls -d PhD*))
 
 for FILE in ${FILELIST[@]}
 do
